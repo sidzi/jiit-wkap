f988e1e1e22b80814e84ceceeb05389485779a9c
sidzi <rahulgupta388@gmail.com>
2016-03-21 23:41:23 +0530

    GUI Enhncements <create issue #1 excel file writing is the bottleneck please improve it>

--- app.py
+++ app.py
@@ -1,33 +1,41 @@
-from easygui import passwordbox, enterbox
 from splinter import Browser
 from utils import fileReader, fileWriter
+import easygui
 import automationModule
+from bs4 import BeautifulSoup
 
 (member_code, password) = fileReader.read()
 if member_code is None or member_code is '':
-    member_code = enterbox("Enter Member Code:")
-    password = passwordbox()
+    member_code = easygui.enterbox("Enter Member Code:")
+    password = easygui.passwordbox()
     fileWriter.write(member_code, password)
 
 with Browser() as browser:
-    # Visit URL
     url = "https://webkiosk.jiit.ac.in/"
     browser.visit(url)
     browser.find_by_id('UserType').first.select('E')
     browser.fill('MemberCode', member_code)
     browser.fill('Password', password)
-
-    # Find and click the 'search' button
     button = browser.find_by_name('BTNSubmit')
-    # Interact with elements
     button.click()
-    if browser.status_code == 200 and not "Wrong Member" in browser.html:
+
+    if browser.status_code == 200 and "Wrong Member" not in browser.html:
         browser.visit("https://webkiosk.jiit.ac.in/EmployeeFiles/AcademicInfo/ViewAttendanceSummary11.jsp?SrcType=I")
-        browser.find_by_id('Exam').first.select('2016EVESEM')
-        browser.find_by_id('Subject').first.select('140076')
+
+        exam_choices = BeautifulSoup(browser.html, "html.parser").find(id='Exam').find_all("option")
+        exam_choices_list = list(exam_choice.attrs['value'] for exam_choice in exam_choices)
+        exam_choice_selected = easygui.choicebox(choices=exam_choices_list)
+        browser.find_by_id('Exam').first.select(exam_choice_selected)
+
+        subject_choices = BeautifulSoup(browser.html, "html.parser").find(id='Subject').find_all("option")
+        subject_choices_list = list(choice.attrs['value'] for choice in subject_choices)
+        subject_choice_selected = easygui.choicebox(choices=subject_choices_list)
+        browser.find_by_id('Subject').first.select(subject_choice_selected)
+
         button = browser.find_by_value('Show/Refresh')
         button.click()
+
         if browser.status_code == 200:
-            automationModule.activate(browser.html, attendancePull=True)
+            automationModule.activate(browser.html, attendancePull=True, fileNameID=subject_choice_selected)
     else:
         print("Error!?!?!?")
